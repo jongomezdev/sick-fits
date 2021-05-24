2de80ffa387daf62c1ef7a00eeda5cbc5aa36f17
Jonathan Gomez <jongomezdev@gmail.com>
2021-05-24 14:56:02 -0400

    fix and style the navbar

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,12 +1,13 @@
 import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
 export default function Nav() {
   return (
-    <nav>
+    <NavStyles>
       <Link href="/products">products</Link>
       <Link href="/sell">sell</Link>
       <Link href="/orders">orders</Link>
       <Link href="/account">account</Link>
-    </nav>
+    </NavStyles>
   );
 }
