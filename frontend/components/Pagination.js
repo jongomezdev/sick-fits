575e0e21622894faac8b64a372a67bd22110ac4a
Jonathan Gomez <jongomezdev@gmail.com>
2021-07-28 13:03:56 -0400

    test: pagination dynamic routing

--- frontend/components/Pagination.js
+++ frontend/components/Pagination.js
@@ -25,15 +25,15 @@ export default function Pagination({ page }) {
       <Head>
         <title>Sick Fits - Page {page} of ___</title>
       </Head>
-      <Link href={`/>products/${page - 1}`}>
+      <Link href={`/products/${page - 1}`}>
         <a aria-disabled={page <= 1}>Prev</a>
       </Link>
       <p>
         Page {page} of {pageCount}
       </p>
       <p>{count} Items Total</p>
-      <Link href={`/>products/${page + 1}`}>
-        <a aria-disabled={page > pageCount}>Next</a>
+      <Link href={`/products/${page + 1}`}>
+        <a aria-disabled={page >= pageCount}>Next</a>
       </Link>
     </PaginationStyles>
   );
---
+++ frontend/pages/products/index.js
@@ -0,0 +1,16 @@
+import { useRouter } from 'next/dist/client/router';
+import Pagination from '../../components/Pagination';
+import Products from '../../components/Products';
+
+export default function ProductPage() {
+  const { query } = useRouter();
+  console.log(query);
+  const page = parseInt(query.page);
+  return (
+    <div>
+      <Pagination page={page || 1} />
+      <Products />
+      <Pagination page={page || 1} />
+    </div>
+  );
+}
